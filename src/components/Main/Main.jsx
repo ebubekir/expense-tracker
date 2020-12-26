785e2b3cc6364184c185027644af47cb7ee79ca7
Ebubekir Karanfil <karanfilebubekir@gmail.com>
2020-12-27 00:14:02 +0300

    Add List component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 import useStyles from './styles';
 import Form from './Form/Form';
+import List from './List/List';
 const Main = () => {
     const classes = useStyles();
     return (
@@ -19,7 +20,7 @@ const Main = () => {
             <CardContent className={classes.cardContent}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
-                        {/* <List /> */}
+                       <List />
                     </Grid>                    
                 </Grid>
             </CardContent>
